9086097c23fd660ac3209e2b2a3ec46417a72dbe
A.J. May <aj7may@gmail.com>
2017-08-14 23:08:03 -0400

    styles

--- js/main.js
+++ js/main.js
@@ -5,13 +5,6 @@ const downloadLink = document.getElementById('download-link');
 const ctx = canvas.getContext("2d");
 
 function updateCanvas() {
-  canvas.width = magoonPhoto.width;
-  canvas.height = magoonPhoto.height;
-  ctx.fillStyle = 'white';
-  ctx.font = '36pt impact';
-  ctx.lineWidth = 10;
-  ctx.miterLimit=2;
-  ctx.textAlign = "center";
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx.drawImage(magoonPhoto, 0, 0);
   ctx.strokeText(captionForm.top.value.toUpperCase(), canvas.width/2, 80);
@@ -21,5 +14,22 @@ function updateCanvas() {
   downloadLink.href = canvas.toDataURL();
 }
 
-magoonPhoto.addEventListener('load', updateCanvas);
-updateCanvas();
+function initCanvas() {
+  canvas.width = magoonPhoto.width;
+  canvas.height = magoonPhoto.height;
+
+  ctx.fillStyle = 'white';
+  ctx.font = '34pt HelveticaNeue-CondensedBlack';
+  ctx.lineWidth = 10;
+  ctx.miterLimit=2;
+  ctx.textAlign = "center";
+
+  updateCanvas();
+}
+
+if (magoonPhoto.complete) {
+  initCanvas();
+} else {
+  magoonPhoto.addEventListener('load', initCanvas);
+}
+
